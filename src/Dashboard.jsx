6976fb73ffa139889ddb6614b792bd9b290a80c9
sophienocode <sophieprotanki@gmail.com>
2026-02-15 08:53:32 -0800

    (Auth): casual update

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -7,12 +7,12 @@ export default function Dashboard() {
 
   return (
     <>
-      <h1>Dashboard the website!</h1>
-      <h2>think in some ideas</h2>
+    <h1>Dashboard the website!</h1>
+      
        <h3>maybe a calendar?</h3>
        <h4>Ola ola oal ola ola ola</h4>
        <h1>helo world</h1>
-       <h2>smth good today</h2>
+  
       
 
     </>
