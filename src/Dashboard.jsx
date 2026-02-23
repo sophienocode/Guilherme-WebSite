81f6a1918975cabca7940713b5b1a04372b76d89
sophienocode <sophieprotanki@gmail.com>
2026-02-23 09:39:10 -0800

    update

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -12,7 +12,28 @@ export default function Dashboard() {
        <h3>maybe a calendar?</h3>
        <h4>Ola ola oal ola ola ola</h4>
        <h1>helo world</h1>
+       <h1>Dashboard the website!</h1>
+      
+       <h3>maybe a calendar?</h3>
+       <h4>Ola ola oal ola ola ola</h4>
+       <h1>helo world</h1>
 
+       <h4>ola ola ola </h4>
+
+       <h1>Dashboard the website!</h1>
+      
+       <h3>maybe a calendar?</h3>
+       <h4>Ola ola oal ola ola ola</h4>
+       <h1>helo world</h1>
+
+       <h4>ola ola ola </h4>
+       <h1>Dashboard the website!</h1>
+      
+       <h3>maybe a calendar?</h3>
+       <h4>Ola ola oal ola ola ola</h4>
+       <h1>helo world</h1>
+
+       <h4>ola ola ola </h4>
        <h4>ola ola ola </h4>
   
   
